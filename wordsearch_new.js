d1f9184690279a5eefbf374c849944a461d50e84
paulthesloth <kevin.tiede@gmail.com>
2016-11-15 14:37:32 -0500

    Add files via upload

--- wordsearch_new.js
+++ wordsearch_new.js
@@ -574,7 +574,7 @@
 	  if(document.getElementById(words[0]).getAttribute("text-decoration") == 'none') {
 		var currentscore = score();
 		var found_words = found_words.push(words[0]) // ggyjugfykgfvyifvyf
-	  	document.getElementById("score").innerHTML = "Found " + currentscore + " out of " + found_words +this.settings.words.length + " words so far.";
+	  	document.getElementById("score").innerHTML = "Found " + currentscore + " 123456789out of " + found_words +this.settings.words.length + " words so far.";
 		document.getElementById(words[0]).style.setProperty("text-decoration", "line-through");
 		document.getElementById(words[0]).setAttribute("text-decoration", "line-through");
 		
